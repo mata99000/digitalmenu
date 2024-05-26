a3f258b6eb7e6b090ad601b4cd4c32deac5931a4
MATA <tatamata1405@gmail.com>
2024-05-26 12:51:37 +0200

    category list update

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,7 +6,7 @@
     <title>Mateja&#x27;s Stupendous Site</title>
     <meta content="width=device-width, initial-scale=1" name="viewport"/>
     <meta content="Webflow" name="generator"/>
-    <link href="{{ asset('storage/css/style.css') }}" rel="stylesheet" type="text/css"/>
+    <link href="{{ asset('style.css') }}" rel="stylesheet" type="text/css"/>
     <link href="https://fonts.googleapis.com" rel="preconnect"/>
     <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin="anonymous"/>
     <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" type="text/javascript"></script>
@@ -46,8 +46,7 @@
     </div>
     @livewire('category-list')
 
-    <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=66413335a09102da6c5071bb" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-    @livewireScripts
-    <script src="{{ asset('storage/js/app.js') }}" type="text/javascript"></script>
+   @livewireScripts
+    <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=66413335a09102da6c5071bb" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script><script src="https://assets-global.website-files.com/66413335a09102da6c5071bb/js/webflow.d035cdcb0.js" type="text/javascript"></script>
 </body>
 </html>
