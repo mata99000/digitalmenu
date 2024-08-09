f6de924986e16a1ee6afac8a93bed54223390f91
Mateja <tatamata1405@gmail.com>
2024-08-09 17:32:56 +0200

    Composer Update

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://laravel.local'),
+    'url' => env('APP_URL', 'http://digitalmenu.test'),
 
     /*
     |--------------------------------------------------------------------------
